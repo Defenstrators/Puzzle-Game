0761e791f98dcef4b180960b9a1009e7e106a5b7
MrKnighty <dooty2nd@hotmail.com>
2021-08-31 20:16:31 +0930

    added materials, working on main menu
    added chared wall and roof tiles.
    imported elevator
    finished lighting in level 1
    working on main menu script

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void SwitchMenu(int menu) // 1 is main, 2 is settings, 3 is level select.
+    {
+        switch (menu)
+        {
+            case 1:
+
+            break;
+        }
+
+
+        }
+    }
+
+
+}
