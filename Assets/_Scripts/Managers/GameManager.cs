2827523b8b220f5b576cd7d852d9a801f1b50347
Bennjmann <Bennjmann.red@gmail.com>
2021-09-02 18:27:27 +1000

    Revert "Added scene Picker"
    This reverts commit b9df5e69b24397a516e88b22366dc8d410f8c354.

--- Assets/_Scripts/Managers/GameManager.cs
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -6,14 +6,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
 using AsyncOperation = UnityEngine.AsyncOperation;
-using Scene = UnityEditor.SearchService.Scene;
 
 public class GameManager : MonoBehaviour {
-    public static GameManager instance;  // SingleTon Lazy and Unsafe But Works stably
-    public GameObject loadingScreen;   // Transition.
-    private int _CurrentScene;       // Use to switch from level 1 and on.
-    private int sceneRemoval = 1;  // Scene to be removed.
-
+    public static GameManager instance; // SingleTon Lazy and Unsafe But Works stably
+    public GameObject loadingScreen;
+    private int _CurrentScene; // Use to switch from level 1 and on.
+    private int sceneRemoval;
+    
+    private bool delay;
+    
     public Animator transitionAnimator;
 
     private void Awake() {
@@ -22,26 +23,17 @@ public class GameManager : MonoBehaviour {
     }
 
     private List<AsyncOperation> _ScenesLoading = new List<AsyncOperation>(); // List Of Scenes Loading And Unloading During Loading Screen.
-    /// <summary>
-    /// Play Button Function
-    /// </summary>
     public void LoadGame() { // Loads Level One.
-        sceneRemoval = 1;
+        sceneRemoval = _CurrentScene + 1;
         _CurrentScene = 2;
         loadingScreen.SetActive(true);
-        StartCoroutine(Loading(0));
-        
+        StartCoroutine(Loading());
         // _ScenesLoading.Add(SceneManager.UnloadSceneAsync((int)SceneIndexes.TITLE_SCREEN));
         //     _ScenesLoading.Add(SceneManager.LoadSceneAsync((int)SceneIndexes.LEVEL1, LoadSceneMode.Additive));
         //     StartCoroutine(GetSceneLoadProgress());
         
 
-    }   
-    
-    /// <summary>
-    /// Gets Current Loading progress
-    /// </summary>
-    /// <returns> Nothing </returns>
+    }
     public IEnumerator GetSceneLoadProgress() {
         for (int i = 0; i < _ScenesLoading.Count; i++) {
             while (!_ScenesLoading[i].isDone) {
@@ -53,41 +45,23 @@ public class GameManager : MonoBehaviour {
         
     }
     
-/// <summary>
-/// Loads The next Level Vira scene Index.
-/// </summary>
+
     public void LoadNextLevel() {      
         sceneRemoval = _CurrentScene;
         _CurrentScene++;
         loadingScreen.gameObject.SetActive(true);
-        StartCoroutine(Loading(0));
+        StartCoroutine(Loading());
         //NOTE Ending Level animations screen can be Input here (loading Scene With animations playing in it. PLayer may have to be Moved for this Interaction) and Loading screen can be removed When animations is used.
+        
     }
 
-/// <summary>
-/// Use to Pick a scene.
-/// </summary>
-/// <param name="i"></param>
-    public void ScenePicker(int i) {
-        StartCoroutine(Loading(i));
-        _CurrentScene = i;
-    }
-
-    /// <summary>
-    /// Plays transition, Unloads Current, loads next scene, Plays exit transition.
-    /// </summary>
-    /// <param name="i"></param>
-    /// <returns> Wait Time. </returns>
-    private IEnumerator Loading(int i) {
+    private IEnumerator Loading() {
         Debug.Log(sceneRemoval);
         Debug.Log(_CurrentScene);
         transitionAnimator.Play("Close");
-        
         yield return new WaitForSeconds(transitionAnimator.GetCurrentAnimatorStateInfo(0).length + 1f);
         _ScenesLoading.Add(SceneManager.UnloadSceneAsync((int)sceneRemoval));
-        if (i > 0.5f) {
-            _ScenesLoading.Add(SceneManager.LoadSceneAsync(i, LoadSceneMode.Additive));
-        } else _ScenesLoading.Add(SceneManager.LoadSceneAsync((int)_CurrentScene, LoadSceneMode.Additive));
+        _ScenesLoading.Add(SceneManager.LoadSceneAsync((int)_CurrentScene, LoadSceneMode.Additive));
         StartCoroutine(GetSceneLoadProgress());
         transitionAnimator.Play("Open");
     }
