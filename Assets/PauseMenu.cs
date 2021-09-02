aeefed351d9561810c2e618b92f7dde01b90b561
MrKnighty <dooty2nd@hotmail.com>
2021-09-02 18:24:32 +0930

    pause menu
    added pause menu, updated camera controller to stop moving when paused.
    added canvas prefab.
    fixed some lighting bugs in level 1

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] CameraController cc;
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {
-            
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+            cc.cutScene = true;
+            Cursor.lockState = CursorLockMode.None;
         }
+    }
+
+
+    public void UnPause()
+    {
+        Time.timeScale = 1; 
+        pauseMenu.SetActive(false);
+        cc.cutScene = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
